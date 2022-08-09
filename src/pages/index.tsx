13ce7c124b87f12b4d56143010d6bf2328b343d9
Fábio Mantelli <fmmantelli@gmail.com>
2022-08-09 16:08:35 -0300

    fix: height is correct

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
-import Header from '../components/Header'
-import MainSection from '../components/MainSection'
-import RightSide from '../components/RightSide'
-import { Box, Flex } from '@chakra-ui/react'
-import Div100vh from 'react-div-100vh'
+import Header from "../components/Header";
+import MainSection from "../components/MainSection";
+import RightSide from "../components/RightSide";
+import { Box, Flex } from "@chakra-ui/react";
+import Div100vh from "react-div-100vh";
 
 const Home: NextPage = () => {
   return (
     <Div100vh>
-      <Box
-        backgroundImage='/background.webp'
-        backgroundSize='cover'
-        // height='100vh'
-        width='100vw'
-      >
-        <Header logo='white' />
-        <MainSection logo='white' />
-      </Box>
-      <RightSide logo='white' />
+      <Flex>
+        <Box
+          backgroundImage="/background.webp"
+          backgroundSize="cover"
+          // height='100vh'
+          width="100vw"
+        >
+          <Header logo="white" />
+          <MainSection logo="white" />
+        </Box>
+        <RightSide logo="white" />
+      </Flex>
     </Div100vh>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
