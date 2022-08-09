92bac823aad83b695f290866accc031a93aae67f
unknown <fmmantelli@gmail.com>
2022-08-08 21:15:25 -0300

    fix: removed / from logo and whitelogo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,8 +13,6 @@ import {
 
 import { Slant as Hamburger } from "hamburger-react";
 
-import Logo from '/logo.svg'
-import WhiteLogo from '/whitelogo.svg'
 
 interface LogoProps {
   logo: string
@@ -31,7 +29,7 @@ function Header({ logo }: LogoProps) {
     >
       <Flex alignItems="center" textDecor='none'>
         <Link href="/">
-          <Image src={logo == 'white' ? WhiteLogo : Logo} width='100%' height='100%' />
+          <Image src={logo == 'white' ? '/whitelogo.svg' : '/white.svg'} width='100%' height='100%' />
         </Link>
         <Spacer />
         {
