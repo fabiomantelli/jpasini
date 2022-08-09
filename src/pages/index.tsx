b7827eaf7e4355e337668ea6f3f84914dace481e
Fábio Mantelli <fmmantelli@gmail.com>
2022-08-09 16:11:39 -0300

    fix: yello border

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,22 @@ import Header from "../components/Header";
 import MainSection from "../components/MainSection";
 import RightSide from "../components/RightSide";
 import { Box, Flex } from "@chakra-ui/react";
-import Div100vh from "react-div-100vh";
 
 const Home: NextPage = () => {
   return (
-    <Div100vh>
-      <Flex>
-        <Box
-          backgroundImage="/background.webp"
-          backgroundSize="cover"
-          // height='100vh'
-          width="100vw"
-        >
-          <Header logo="white" />
-          <MainSection logo="white" />
-        </Box>
-        <RightSide logo="white" />
-      </Flex>
-    </Div100vh>
+    <Flex>
+      <Box
+        backgroundImage="/background.webp"
+        backgroundSize="cover"
+        height='100vh'
+        width="100vw"
+        border="2px solid yellow"
+      >
+        <Header logo="white" />
+        <MainSection logo="white" />
+      </Box>
+      <RightSide logo="white" />
+    </Flex>
   );
 };
 
