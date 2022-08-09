fd28675a8e10da9bbd449a8f0d84bed49194a43f
Fábio Mantelli <fmmantelli@gmail.com>
2022-08-09 17:26:42 -0300

    fix: height fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,20 @@ import MainSection from "../components/MainSection";
 import RightSide from "../components/RightSide";
 import { Box, Flex } from "@chakra-ui/react";
 
-import { ViewportHeightBox } from "next-real-viewport";
-
 const Home: NextPage = () => {
   return (
-    <ViewportHeightBox>
-      <Flex>
-        <Box
-          backgroundImage="/background.webp"
-          backgroundSize="cover"
-          width="100%"
-          height="vh"
-        >
-          <Header logo="white" />
-          <MainSection logo="white" />
-        </Box>
-        <RightSide logo="white" />
-      </Flex>
-    </ViewportHeightBox>
+    <Flex>
+      <Box
+        backgroundImage="/background.webp"
+        backgroundSize="cover"
+        width="100%"
+        height="vh"
+      >
+        <Header logo="white" />
+        <MainSection logo="white" />
+      </Box>
+      <RightSide logo="white" />
+    </Flex>
   );
 };
 
