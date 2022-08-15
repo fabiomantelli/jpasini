5e53a272e2bc8254517585785efae73a1ee6f315
unknown <fmmantelli@gmail.com>
2022-08-14 21:18:10 -0300

    fix: changed assets images

--- src/data/enterprises.ts
+++ src/data/enterprises.ts
@@ -3,7 +3,7 @@ export const enterprisesData = [
    name: "Antônio Emílio",
    mainDescription: "Projeto com 46 Unidades, todos os apartamentos de 2 quartos, sendo 1 suíte.",
    secondaryDescription: "Possui sacada com churrasqueira a carvão com bancada de granito, 1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
-   url: "/background-antonio-emilio.webp",
+   url: "/assets/background-antonio-emilio.webp",
    logoColor: "white",
    textColor: "white",
    colorMenu: "white"
@@ -12,7 +12,7 @@ export const enterprisesData = [
    name: "Residencial Viena",
    mainDescription: "Projeto com 30 Unidades, todos os apartamentos de 3 quartos, sendo 2 suíte.",
    secondaryDescription: "1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
-   url: "/background-viena.webp",
+   url: "/assets/background-viena.webp",
    logoColor: "green",
    textColor: "white",
    colorMenu: "white"
@@ -21,7 +21,7 @@ export const enterprisesData = [
    name: "Internacional",
    mainDescription: "Garagens, Salas Comerciais e spa.",
    secondaryDescription: "3 vaga de garagem, dois elevadores, guarita, segurança e serviço de TV.",
-   url: "/background-internacional.webp",
+   url: "/assets/background-internacional.webp",
    logoColor: "green",
    textColor: "green",
    colorMenu: "green"
