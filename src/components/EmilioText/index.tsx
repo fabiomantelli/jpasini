4d265cfee03ae3d63721327ab0e88d22b2872c41
Fábio Mantelli <fmmantelli@gmail.com>
2022-08-15 15:40:26 -0300

    fix: removed comments into EmilioText Component

--- src/components/EmilioText/index.tsx
+++ src/components/EmilioText/index.tsx
@@ -6,7 +6,6 @@ const moveTextUp = keyframes`
     transform: translateY(30px);
   }
   100% {
-    /* transform: translateY(0); */
     opacity: 1;
   }
 `;
@@ -17,7 +16,6 @@ const moveTextDown = keyframes`
   }
 
   100% {
-    /* transform: translateY(0); */
     opacity: 1;
   }
 `;
