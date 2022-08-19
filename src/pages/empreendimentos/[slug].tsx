2a6f20536f6723c4c87bf3a7b556ed030c354b56
unknown <fmmantelli@gmail.com>
2022-08-18 22:47:58 -0300

    fix: added interrogation to images

--- src/pages/empreendimentos/[slug].tsx
+++ src/pages/empreendimentos/[slug].tsx
@@ -80,12 +80,12 @@ function Empreendimentos({ enterprise }: EnterpriseProps) {
       />
 
       <SectionTwo
-        imageOne={enterprise[0].images.find((image) => image.id == 1)!}
-        imageOneMobile={enterprise[0].images.find((image) => image.id == 1)!}
-        imageTwo={enterprise[0].images.find((image) => image.id == 2)!}
-        imageTwoMobile={enterprise[0].images.find((image) => image.id == 2)!}
-        imageThree={enterprise[0].images.find((image) => image.id == 3)!}
-        imageThreeMobile={enterprise[0].images.find((image) => image.id == 301)!}
+        imageOne={enterprise[0].images?.find((image) => image.id == 1)!}
+        imageOneMobile={enterprise[0].images?.find((image) => image.id == 1)!}
+        imageTwo={enterprise[0].images?.find((image) => image.id == 2)!}
+        imageTwoMobile={enterprise[0].images?.find((image) => image.id == 2)!}
+        imageThree={enterprise[0].images?.find((image) => image.id == 3)!}
+        imageThreeMobile={enterprise[0].images?.find((image) => image.id == 301)!}
         side={0}
         textDescription={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -93,30 +93,30 @@ function Empreendimentos({ enterprise }: EnterpriseProps) {
       />
 
       <SectionThree
-        imageOne={enterprise[0].images.find((image) => image.id == 4)!}
-        imageTwo={enterprise[0].images.find((image) => image.id == 5)!}
-        imageTwoMobile={enterprise[0].images.find((image) => image.id == 501)!}
-        imageThree={enterprise[0].images.find((image) => image.id == 6)!}
+        imageOne={enterprise[0].images?.find((image) => image.id == 4)!}
+        imageTwo={enterprise[0].images?.find((image) => image.id == 5)!}
+        imageTwoMobile={enterprise[0].images?.find((image) => image.id == 501)!}
+        imageThree={enterprise[0].images?.find((image) => image.id == 6)!}
       />
 
       <SectionFour
-        imageOne={enterprise[0].images.find((image) => image.id == 7)!}
-        imageOneMobile={enterprise[0].images.find((image) => image.id == 7)!}
-        imageTwo={enterprise[0].images.find((image) => image.id == 8)!}
-        imageTwoMobile={enterprise[0].images.find((image) => image.id == 801)!}
-        imageThree={enterprise[0].images.find((image) => image.id == 9)!}
-        imageThreeMobile={enterprise[0].images.find((image) => image.id == 901)!}
+        imageOne={enterprise[0].images?.find((image) => image.id == 7)!}
+        imageOneMobile={enterprise[0].images?.find((image) => image.id == 7)!}
+        imageTwo={enterprise[0].images?.find((image) => image.id == 8)!}
+        imageTwoMobile={enterprise[0].images?.find((image) => image.id == 801)!}
+        imageThree={enterprise[0].images?.find((image) => image.id == 9)!}
+        imageThreeMobile={enterprise[0].images?.find((image) => image.id == 901)!}
         imageLogo={enterprise[0].imageLogo}
         side={0}
       />
 
       <SectionTwo
-        imageOne={enterprise[0].images.find((image) => image.id == 10)!}
-        imageOneMobile={enterprise[0].images.find((image) => image.id == 1001)!}
-        imageTwo={enterprise[0].images.find((image) => image.id == 11)!}
-        imageTwoMobile={enterprise[0].images.find((image) => image.id == 1101)!}
-        imageThree={enterprise[0].images.find((image) => image.id == 12)!}
-        imageThreeMobile={enterprise[0].images.find((image) => image.id == 1201)!}
+        imageOne={enterprise[0].images?.find((image) => image.id == 10)!}
+        imageOneMobile={enterprise[0].images?.find((image) => image.id == 1001)!}
+        imageTwo={enterprise[0].images?.find((image) => image.id == 11)!}
+        imageTwoMobile={enterprise[0].images?.find((image) => image.id == 1101)!}
+        imageThree={enterprise[0].images?.find((image) => image.id == 12)!}
+        imageThreeMobile={enterprise[0].images?.find((image) => image.id == 1201)!}
         side={1}
         textDescription={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -124,12 +124,12 @@ function Empreendimentos({ enterprise }: EnterpriseProps) {
       />
 
       <SectionFour
-        imageOne={enterprise[0].images.find((image) => image.id == 13)!}
-        imageOneMobile={enterprise[0].images.find((image) => image.id == 13)!}
-        imageTwo={enterprise[0].images.find((image) => image.id == 14)!}
-        imageTwoMobile={enterprise[0].images.find((image) => image.id == 1401)!}
-        imageThree={enterprise[0].images.find((image) => image.id == 15)!}
-        imageThreeMobile={enterprise[0].images.find((image) => image.id == 1501)!}
+        imageOne={enterprise[0].images?.find((image) => image.id == 13)!}
+        imageOneMobile={enterprise[0].images?.find((image) => image.id == 13)!}
+        imageTwo={enterprise[0].images?.find((image) => image.id == 14)!}
+        imageTwoMobile={enterprise[0].images?.find((image) => image.id == 1401)!}
+        imageThree={enterprise[0].images?.find((image) => image.id == 15)!}
+        imageThreeMobile={enterprise[0].images?.find((image) => image.id == 1501)!}
         imageLogo={enterprise[0].imageLogo}
         side={1}
       />
