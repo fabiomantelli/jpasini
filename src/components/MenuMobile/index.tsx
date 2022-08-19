ff5d7369c1418780beb03448c1f44192c51421ec
Fábio Mantelli <fmmantelli@gmail.com>
2022-08-19 15:00:21 -0300

    fix: transition width and height .4s

--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -15,7 +15,7 @@ function MenuMobile({ menuIsVisible, setMenuIsVisible, logoColor }: MenuProps) {
       position="absolute"
       right={0}
       top={0}
-      transition='0.4s'
+      transition='width 0.4s, height 0.4s'
       willChange='width, height'
       height={menuIsVisible ? "calc(100% - 20px)" : "60px"}
       width={
