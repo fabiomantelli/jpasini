f70e1d2a48696d54f9c6d5dc8fa9f59b9703f308
unknown <fmmantelli@gmail.com>
2022-08-18 22:52:22 -0300

    fix: enterprises file is correct

--- src/data/enterprises.ts
+++ src/data/enterprises.ts
@@ -9,7 +9,7 @@ export const enterprisesData = [
     mainDescription:
       "Projeto com 46 Unidades, todos os apartamentos de 2 quartos, sendo 1 suíte.",
     secondaryDescription:
-      "Possui sacada com churrasqueira a carvão com bancada de granito, 1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
+      "Possui 1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
     url: "/assets/background-antonio-emilio.webp",
     imageLogo: {
       url: "../assets/antonioemilio/antonio-emilio-logo.svg",
@@ -227,37 +227,455 @@ export const enterprisesData = [
   },
   {
     name: "Residencial Viena",
-    date: 2023,
-    location: "Rua das Gaivotas, xxx",
-    floors: 5,
-    status: "Em construção",
+    date: 2025,
     typeOfEnterprise: "Residencial",
+    location: "R. Getúlio Vargas, 444",
+    floors: 8,
+    status: "Em construção",
     mainDescription:
-      "Projeto com 30 Unidades, todos os apartamentos de 3 quartos, sendo 2 suíte.",
+      "Projeto com 20 Unidades, todos os apartamentos de 2 quartos, sendo 1 suíte.",
     secondaryDescription:
-      "1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
+      "Possui sacada, 1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
     url: "/assets/background-viena.webp",
-    urlLogo: "../assets/antonioemilio/antonio-emilio-logo.svg",
-    path: "residencial-viena",
-    logoColor: "green",
+    imageLogo: {
+      url: "../assets/antonioemilio/antonio-emilio-logo.svg",
+      alt: "Antônio Emílio Logo"
+    },
+    mainTitle: "Prepare-se para viver com conforto no litoral catarinense.",
+    mainSubTitle:
+      "São 46 apartamentos de 2 quartos, sendo 1 suíte. Sacada com churrasqueira a carvão e bancada de granito. Possui 1 vaga de garagem, salão de festas, 2 elevadores, guarita, segurança e serviço de TV.",
+    path: "antonio-emilio",
+    logoColor: "white",
     textColor: "white",
     colorMenu: "white",
+    images: [
+      {
+        id: 1,
+        url: "../assets/antonioemilio/image01.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 101,
+        url: "../assets/antonioemilio/image01-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2,
+        url: "../assets/antonioemilio/image02.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 3,
+        url: "../assets/antonioemilio/image03.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 3,
+        url: "../assets/antonioemilio/image03.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 301,
+        url: "../assets/antonioemilio/image03-mobile.webp",
+        alt: "Antônio Emílio Mobile",
+      },
+      {
+        id: 4,
+        url: "../assets/antonioemilio/image04.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 5,
+        url: "../assets/antonioemilio/image05.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 501,
+        url: "../assets/antonioemilio/image05-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 6,
+        url: "../assets/antonioemilio/image06.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 7,
+        url: "../assets/antonioemilio/image07.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 701,
+        url: "../assets/antonioemilio/image07-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 8,
+        url: "../assets/antonioemilio/image08.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 801,
+        url: "../assets/antonioemilio/image08-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 9,
+        url: "../assets/antonioemilio/image09.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 901,
+        url: "../assets/antonioemilio/image09-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 10,
+        url: "../assets/antonioemilio/image10.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1001,
+        url: "../assets/antonioemilio/image10-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 11,
+        url: "../assets/antonioemilio/image11.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1101,
+        url: "../assets/antonioemilio/image11-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 12,
+        url: "../assets/antonioemilio/image12.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1201,
+        url: "../assets/antonioemilio/image12-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 13,
+        url: "../assets/antonioemilio/image13.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1301,
+        url: "../assets/antonioemilio/image13-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 14,
+        url: "../assets/antonioemilio/image14.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1401,
+        url: "../assets/antonioemilio/image14-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 15,
+        url: "../assets/antonioemilio/image15.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1501,
+        url: "../assets/antonioemilio/image15-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 16,
+        url: "../assets/antonioemilio/image16.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1601,
+        url: "../assets/antonioemilio/image16-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 17,
+        url: "../assets/antonioemilio/image17.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1701,
+        url: "../assets/antonioemilio/image17-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 18,
+        url: "../assets/antonioemilio/image18.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1801,
+        url: "../assets/antonioemilio/image18-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 19,
+        url: "../assets/antonioemilio/image19.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1901,
+        url: "../assets/antonioemilio/image19-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 20,
+        url: "../assets/antonioemilio/image20.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2001,
+        url: "../assets/antonioemilio/image20-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 21,
+        url: "../assets/antonioemilio/image21.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2101,
+        url: "../assets/antonioemilio/image21-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+    ],
   },
   {
     name: "Internacional",
     date: 2024,
-    location: "Rua Getúlio Vargas, xxx",
-    floors: 12,
+    typeOfEnterprise: "Residencial",
+    location: "R. São José",
+    floors: 8,
     status: "Em construção",
-    typeOfEnterprise: "Comercial",
-    mainDescription: "Garagens, Salas Comerciais e spa.",
+    mainDescription:
+      "Projeto com 400 Unidades, todos os apartamentos de 2 quartos, sendo 1 suíte.",
     secondaryDescription:
-      "3 vaga de garagem, dois elevadores, guarita, segurança e serviço de TV.",
+      "Possui sacada com churrasqueira a carvão com bancada de granito, 1 vaga de garagem, salão de festas, dois elevadores, guarita, segurança e serviço de TV.",
     url: "/assets/background-internacional.webp",
-    urlLogo: "../assets/antonioemilio/antonio-emilio-logo.svg",
-    path: "internacional",
-    logoColor: "green",
-    textColor: "green",
-    colorMenu: "green",
+    imageLogo: {
+      url: "../assets/antonioemilio/antonio-emilio-logo.svg",
+      alt: "Antônio Emílio Logo"
+    },
+    mainTitle: "Prepare-se para viver com conforto no litoral catarinense.",
+    mainSubTitle:
+      "São 46 apartamentos de 2 quartos, sendo 1 suíte. Sacada com churrasqueira a carvão e bancada de granito. Possui 1 vaga de garagem, salão de festas, 2 elevadores, guarita, segurança e serviço de TV.",
+    path: "antonio-emilio",
+    logoColor: "white",
+    textColor: "white",
+    colorMenu: "white",
+    images: [
+      {
+        id: 1,
+        url: "../assets/antonioemilio/image01.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 101,
+        url: "../assets/antonioemilio/image01-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2,
+        url: "../assets/antonioemilio/image02.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 3,
+        url: "../assets/antonioemilio/image03.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 3,
+        url: "../assets/antonioemilio/image03.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 301,
+        url: "../assets/antonioemilio/image03-mobile.webp",
+        alt: "Antônio Emílio Mobile",
+      },
+      {
+        id: 4,
+        url: "../assets/antonioemilio/image04.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 5,
+        url: "../assets/antonioemilio/image05.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 501,
+        url: "../assets/antonioemilio/image05-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 6,
+        url: "../assets/antonioemilio/image06.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 7,
+        url: "../assets/antonioemilio/image07.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 701,
+        url: "../assets/antonioemilio/image07-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 8,
+        url: "../assets/antonioemilio/image08.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 801,
+        url: "../assets/antonioemilio/image08-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 9,
+        url: "../assets/antonioemilio/image09.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 901,
+        url: "../assets/antonioemilio/image09-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 10,
+        url: "../assets/antonioemilio/image10.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1001,
+        url: "../assets/antonioemilio/image10-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 11,
+        url: "../assets/antonioemilio/image11.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1101,
+        url: "../assets/antonioemilio/image11-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 12,
+        url: "../assets/antonioemilio/image12.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1201,
+        url: "../assets/antonioemilio/image12-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 13,
+        url: "../assets/antonioemilio/image13.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1301,
+        url: "../assets/antonioemilio/image13-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 14,
+        url: "../assets/antonioemilio/image14.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1401,
+        url: "../assets/antonioemilio/image14-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 15,
+        url: "../assets/antonioemilio/image15.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1501,
+        url: "../assets/antonioemilio/image15-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 16,
+        url: "../assets/antonioemilio/image16.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1601,
+        url: "../assets/antonioemilio/image16-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 17,
+        url: "../assets/antonioemilio/image17.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1701,
+        url: "../assets/antonioemilio/image17-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 18,
+        url: "../assets/antonioemilio/image18.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1801,
+        url: "../assets/antonioemilio/image18-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 19,
+        url: "../assets/antonioemilio/image19.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 1901,
+        url: "../assets/antonioemilio/image19-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 20,
+        url: "../assets/antonioemilio/image20.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2001,
+        url: "../assets/antonioemilio/image20-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 21,
+        url: "../assets/antonioemilio/image21.webp",
+        alt: "Antônio Emílio",
+      },
+      {
+        id: 2101,
+        url: "../assets/antonioemilio/image21-mobile.webp",
+        alt: "Antônio Emílio",
+      },
+    ],
   },
+  
 ];
